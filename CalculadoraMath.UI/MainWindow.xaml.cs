fad8e27263e03a5400b6d39f807df1674527bb29
Hector Bonilla <bonilla.osorio0@gmail.com>
2023-06-03 14:42:36 -0600

    Agregando Projecto WPF y Funcional a Solucion

---
+++ CalculadoraMath.UI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CalculadoraMath.BL;
+
+namespace CalculadoraMath.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        CalculadoraAPI api = new CalculadoraAPI();
+
+        private async void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            string Expr = txtExprMath.Text;
+
+            var result = await api.POST(Expr);
+            if (result != null)
+            {
+                txtResult.Text = result;
+            }
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
