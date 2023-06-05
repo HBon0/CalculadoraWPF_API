11799f5c0c9746ab92cea700a8f79eb40e4f75bb
Hector Bonilla <bonilla.osorio0@gmail.com>
2023-06-04 21:59:01 -0600

    Corrigiendo Vista Calculadora

--- CalculadoraMath.UI/MainWindow.xaml.cs
+++ CalculadoraMath.UI/MainWindow.xaml.cs
@@ -42,5 +42,11 @@ namespace CalculadoraMath.UI
         {
             this.Close();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
     }
 }
